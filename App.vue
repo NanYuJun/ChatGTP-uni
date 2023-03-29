bd2d8b491d2eb601b1369ec8310a0a2239dcdf32
王豪豪 <admin@example.com>
2023-03-29 17:01:13 +0800

    新增邀请、广告激励任务、流量主广告、角色模型、内置语句和常见问题、将请求方式兼容stream流方式等

--- App.vue
+++ App.vue
@@ -1,40 +1,58 @@
 <script>
-	import {
-		login
-	} from "./config/login.js";
+	let platform
+	// #ifdef MP-WEIXIN
+	platform = 1
+	// #endif
+	// #ifdef MP-TOUTIAO
+	platform = 2
+	// #endif
+	// #ifdef MP-KUAISHOU
+	platform = 3
+	// #endif
+	// #ifdef H5
+	platform = 4
+	// #endif
 	export default {
+
 		onLaunch: function() {
+
+			let version = "1.0.3"
+			console.log(
+				`\n %c 云AI助手V${version} %c 公众号：南羽工作室 \n\n`,
+				"color: #ffffff; background: #3c9cff; padding:5px 0;",
+				"color: #3c9cff;background: #ffffff; padding:5px 0;"
+			);
+			let oldVersion = uni.getStorageSync('version') || ''
+			if (version != oldVersion) {
+				uni.removeStorageSync('chat')
+				uni.setStorageSync('version', version)
+			}
+			uni.hideTabBar();
 			this.getTabbar();
-			uni.hideTabBar(); // 隐藏tabbar
-			this.getConfig();
+			this.getLoginConfig();
 			this.getRobot();
 			this.getAd();
+			this.getModelList()
 		},
 		methods: {
+			// 获取底部tabbar
 			async getTabbar() {
 				const {
 					data
 				} = await uni.$u.http.post("/app/renovation/tabbar/list");
-				uni.setStorageSync("tabbar", data.data);
-				if (data.data) {
-					let tabbarIndex = uni.getStorageSync('selectedIndex')
-					uni.switchTab({
-						url: tabbarIndex ? data.data[tabbarIndex].pagePath: data.data[0].pagePath
-					});
-				}
+
+				this.$store.commit('setTabbar', data.data?.filter(item => item.platform === platform || item
+					.platform === 0) || [])
+				this.$store.commit('setTabbarIndex', 0)
+				data?.data && uni.switchTab({
+					url: data.data?. [0]?.pagePath
+				})
 			},
-			async getConfig() {
+			async getLoginConfig() {
 				const {
 					data
-				} = await uni.$u.http.get("/app/application/info/config");
-				if (data.code != 1000) {
-					uni.showToast({
-						title: data.message,
-						icon: "none",
-					});
-				}
-				uni.setStorageSync("config", data.data);
-				login();
+				} = await uni.$u.http.get("/app/application/info/loginConfig");
+				uni.setStorageSync("loginConfig", data.data);
 			},
 			// 获取机器人配置
 			async getRobot() {
@@ -45,21 +63,37 @@
 						id: 1,
 					},
 				});
-				uni.setStorageSync("robot", data.data);
+				this.$store.commit('setRobot', data.data)
 			},
+			// 获取广告
 			async getAd() {
 				const {
 					data
 				} = await uni.$u.http.post("/app/ad/info/list");
-				if (data.code != 1000) {
-					uni.showToast({
-						title: data.message,
-						icon: "none",
-					});
-				}
-				uni.setStorageSync("ad", data.data);
+				data.data.forEach(item => {
+					item.desc = item.desc?.replace(
+						/\<p\>&lt;ad(.*?)&gt;&lt;\/ad&gt;\<\/p\>/g,
+						'<ad$1></ad>'
+					);
+				})
+
+				uni.setStorageSync("ad", data.data?.filter(item => item.platform === platform || item
+					.platform === 0) || []);
 			},
-		},
+
+			// 获取模型
+			async getModelList() {
+				const {
+					data
+				} = await uni.$u.http.post('/app/chatgpt/model/list')
+				this.$store.commit('setModelList', data.data)
+				console.log(this.$store.state.model)
+				if (!this.$store.state.model.id) {
+					this.$store.commit('setModel', data.data?. [0] || {})
+				}
+
+			}
+		}
 	};
 </script>
 
@@ -67,8 +101,13 @@
 	/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
 	@import "uview-ui/index.scss";
 	@import "style/common.scss";
+	@import "style/theme.scss";
 
 	page {
+		// background: #353541;
+		// background: #ececec;
+		height: 100%;
 		font-size: 28rpx;
+		font-family: "PingFang SC", "Helvetica Neue", Helvetica, Arial, sans-serif;
 	}
 </style>
--- components/tabbar.vue
+++ components/tabbar.vue
@@ -1,106 +1,89 @@
 <template>
-  <view>
-    <view style="height: 100rpx" v-if="list.length > 1" />
-    <view class="tabbar" v-if="list.length > 1">
-      <view class="navigator">
-        <view
-          class="navigator-item"
-          v-for="(item, index) in list"
-          :key="item.pagePath"
-          @click="switchTab(item, index)"
-        >
-          <img
-            :src="item.iconPath"
-            class="icon"
-            v-if="selectedIndex !== index"
-          />
-          <img :src="item.selectedIconPath" class="icon" v-else />
-          <text
-            :class="['item-text', { 'text-active': selectedIndex === index }]"
-            >{{ item.text }}</text
-          >
-        </view>
-      </view>
-    </view>
-  </view>
+	<view>
+		<view style="height: 100rpx" v-if="tabbar.length > 1" />
+		<view class="tabbar" v-if="tabbar.length > 1">
+			<view class="navigator">
+				<view class="navigator-item" v-for="(item, index) in tabbar" :key="item.pagePath"
+					@click="switchTab(item.pagePath)">
+					<img :src="item.iconPath" class="icon" v-show="tabbarIndex !== index" />
+					<img :src="item.selectedIconPath" class="icon" v-show="tabbarIndex === index"/>
+					<text :class="['item-text', { 'text-active': tabbarIndex === index }]">{{ item.text }}</text>
+				</view>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      list: uni.getStorageSync("tabbar") || [],
-      selectedIndex: uni.getStorageSync("selectedIndex") || 0,
-    };
-  },
-  methods: {
-    setTabbar() {
-      this.list = uni.getStorageSync("tabbar") || [];
-    },
-    switchTab(item, index) {
-      let url = item.pagePath;
-      let pagePath = url;
-      // #ifdef H5
-      if (pagePath.indexOf("http") > -1) {
-        window.open(pagePath);
-        return;
-      }
-      // #endif
-      uni.switchTab({ url });
-      this.list.forEach((v, i) => {
-        if (item.pagePath === v.pagePath) {
-          uni.setStorageSync("selectedIndex", index);
-        }
-      });
-    },
-  },
-};
+	import {
+		mapState,
+		mapMutations
+	} from 'vuex'
+	export default {
+		computed: mapState({
+			// 从state中拿到数据 箭头函数可使代码更简练
+			tabbar: state => state.tabbar,
+			tabbarIndex: state => state.tabbarIndex,
+		}),
+		methods: {
+			...mapMutations([
+				'setTabbarIndex'
+			]),
+			switchTab(url) {
+				let tabbarIndex = this.tabbar.findIndex(item => item.pagePath == url) || 0
+				this.setTabbarIndex(tabbarIndex)
+				uni.$u.route(url)
+			},
+		},
+	};
 </script>
 
 <style>
-.tabbar {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100rpx;
-  display: flex;
-  align-content: center;
-  justify-content: space-between;
-  z-index: 999;
-  background: #f5f5f5;
-  border-top: 2rpx solid #eee;
-}
+	.tabbar {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 100rpx;
+		display: flex;
+		align-content: center;
+		justify-content: space-between;
+		z-index: 999;
+		background: var(--bg-gray);
+		border-top: 2rpx solid var(--border);
+		padding-bottom: constant(safe-area-inset-bottom);
+		padding-bottom: env(safe-area-inset-bottom);
+	}
 
-.navigator {
-  display: flex;
-  flex: 1;
-  justify-content: space-between;
-  align-items: center;
-}
+	.navigator {
+		display: flex;
+		flex: 1;
+		justify-content: space-between;
+		align-items: center;
+	}
 
-.navigator-item {
-  display: flex;
-  justify-content: center;
-  flex: 1;
-  align-items: center;
-  flex-direction: column;
-  width: 50rpx;
-  height: 100%;
-}
+	.navigator-item {
+		display: flex;
+		justify-content: center;
+		flex: 1;
+		align-items: center;
+		flex-direction: column;
+		width: 50rpx;
+		height: 100%;
+	}
 
-.item-text {
-  margin-top: 6rpx;
-  color: #777e86;
-  font-size: 24rpx;
-}
+	.item-text {
+		margin-top: 6rpx;
+		color: #777e86;
+		font-size: 24rpx;
+	}
 
-.text-active {
-  color: #2e92fd !important;
-}
+	.text-active {
+		color: #2e92fd !important;
+	}
 
-.icon {
-  width: 20px;
-  height: 20px;
-}
+	.icon {
+		width: 20px;
+		height: 20px;
+	}
 </style>
